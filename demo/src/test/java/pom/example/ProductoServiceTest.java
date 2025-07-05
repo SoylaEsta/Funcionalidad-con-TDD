d82ede8a53b566f2224124fe40924801740ce934
SoylaEsta <g.sepulvedah@duocuc.cl>
2025-07-04 21:38:17 -0400

    Prueba RED: Falla esperando producto no null

---
+++ demo/src/test/java/pom/example/ProductoServiceTest.java
@@ -0,0 +1,24 @@
+package pom.example; 
+import org.junit.jupiter.api.Test;
+import pom.example.ProductoService;
+import pom.example.Producto;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+ // Este método aún NO existem → el test fallará
+public class ProductoServiceTest {
+
+   
+    @Test
+    void testProductoInexistenteDebeFallar() {
+        ProductoService service = new ProductoService();
+
+        // No se ha creado este producto, por lo tanto debería ser null
+        Producto resultado = service.buscarProductoPorId(99);
+
+        // Este assert va a FALLAR porque estamos forzando una condición falsa
+        assertNotNull(resultado);  // ← RED forzado
+    }
+
+
+}
